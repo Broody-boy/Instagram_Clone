2a5235968da557b6f94814e386a504106ddd309e
Arshdeep <arshdeep2875@gmail.com>
2023-07-16 14:22:24 +0530

    Added switching between Login and Sign Up activity

--- app/src/main/java/com/example/instagramclone/activities/Login.kt
+++ app/src/main/java/com/example/instagramclone/activities/Login.kt
@@ -1,12 +1,23 @@
 package com.example.instagramclone.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.instagramclone.R
+import com.example.instagramclone.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCreateNewAccountRedirect.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/instagramclone/activities/SignUp.kt
+++ app/src/main/java/com/example/instagramclone/activities/SignUp.kt
@@ -3,10 +3,20 @@ package com.example.instagramclone.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.instagramclone.R
+import com.example.instagramclone.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.tvLogInRedirect.setOnClickListener {
+            finish()
+        }
     }
 }
