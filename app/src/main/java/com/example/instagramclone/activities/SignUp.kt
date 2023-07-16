2c72d1ca64abd673ebad21fe0540cfebd3ea0762
Arshdeep <arshdeep2875@gmail.com>
2023-07-16 08:06:03 +0530

    Added Signup Activity

---
+++ app/src/main/java/com/example/instagramclone/activities/SignUp.kt
@@ -0,0 +1,12 @@
+package com.example.instagramclone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagramclone.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
