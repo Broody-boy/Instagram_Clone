6843a8bab1f67ef699bae24b74ec3c94f3be7393
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-07-21 11:55:29 +0530

    Added page for specifiying Username after SignUp

---
+++ app/src/main/java/com/example/instagramclone/activities/UserName.kt
@@ -0,0 +1,12 @@
+package com.example.instagramclone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagramclone.R
+
+class UserName : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_name)
+    }
+}
