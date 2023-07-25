cf2948fc9ee479c88abcd39f797d98dcd08248eb
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-07-25 10:39:47 +0530

    Implemented logic for ChooseProfileImage.kt

--- app/src/main/java/com/example/instagramclone/activities/ChooseProfileImage.kt
+++ app/src/main/java/com/example/instagramclone/activities/ChooseProfileImage.kt
@@ -1,16 +1,138 @@
 package com.example.instagramclone.activities
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.ViewModelProvider
+import com.example.instagramclone.MainActivity
+import com.example.instagramclone.R
+import com.example.instagramclone.Utils
 import com.example.instagramclone.databinding.ActivityChooseProfileImageBinding
+import com.example.instagramclone.mvvm.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayOutputStream
 
 class ChooseProfileImage : AppCompatActivity() {
 
     private lateinit var binding: ActivityChooseProfileImageBinding
+    private lateinit var vm : ViewModel
+    private lateinit var firestore : FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageRef: StorageReference
+    private lateinit var bitmap: Bitmap
+    private var uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChooseProfileImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        vm = ViewModelProvider(this).get(ViewModel::class.java)
+        firestore = FirebaseFirestore.getInstance()
+        storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
+
+        binding.imgGoBack.setOnClickListener { finish() }
+
+        binding.imgsetProfileImage.setOnClickListener {
+            showImageSelectionOptionDialog()
+        }
+
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun showImageSelectionOptionDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.custom_dialog_select_image_options)
+        dialog.window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        dialog.findViewById<Button>(R.id.btnCancel).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.findViewById<LinearLayout>(R.id.layoutTakePicture).setOnClickListener {
+            takePhotoWithCamera()
+            dialog.dismiss()
+        }
+
+        dialog.findViewById<ConstraintLayout>(R.id.layoutSelectFromGallery).setOnClickListener {
+            pickImageFromGallery()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun pickImageFromGallery() {
+        val pickPictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (pickPictureIntent.resolveActivity(this.packageManager) != null) {
+            startActivityForResult(pickPictureIntent, Utils.PROFILE_IMAGE_PICK)
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun takePhotoWithCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, Utils.PROFILE_IMAGE_CAPTURE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                Utils.PROFILE_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    uploadProfile(imageBitmap)
+                }
+                Utils.PROFILE_IMAGE_PICK -> {
+                    val imageUri = data?.data
+                    val imageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+                    uploadProfile(imageBitmap)
+                }
+            }
+        }
+    }
+
+    private fun uploadProfile(imageBitmap: Bitmap?) {
+        val baos = ByteArrayOutputStream()
+        imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        bitmap = imageBitmap!!
+        binding.imgsetProfileImage.setImageBitmap(imageBitmap)
+        val storagePath = storageRef.child("Profile/${Utils.getUidLoggedIn()}.jpg")
+        val uploadTask = storagePath.putBytes(data)
+
+        uploadTask.addOnSuccessListener {
+            val task = it.metadata?.reference?.downloadUrl
+            task?.addOnSuccessListener {
+                uri = it
+                firestore.collection("Users").document(Utils.getUidLoggedIn()).update("image", uri.toString())
+                vm.image.value = uri.toString()
+            }
+            Toast.makeText(this, "Image uploaded successfully!", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed to upload image!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
