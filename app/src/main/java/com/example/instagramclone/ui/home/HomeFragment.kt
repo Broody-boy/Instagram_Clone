bd0498786bc35c10da90df177cb873c8bc619358
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-07-30 10:55:55 +0530

    Added adapter, called loadStories() in HomeFragment.kt

--- app/src/main/java/com/example/instagramclone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/instagramclone/ui/home/HomeFragment.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagramclone.adapters.StoriesAdapter
 import com.example.instagramclone.databinding.FragmentHomeBinding
+import com.example.instagramclone.mvvm.ViewModel
 
 class HomeFragment : Fragment() {
 
@@ -16,6 +21,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var vm : ViewModel
+    private lateinit var adapter_stories : StoriesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +36,19 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vm = ViewModelProvider(this).get(ViewModel::class.java)
+        adapter_stories = StoriesAdapter()
+
+        vm.loadStories().observe(viewLifecycleOwner, Observer {
+            binding.recyclerStories.adapter = adapter_stories
+            binding.recyclerStories.layoutManager= LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+            adapter_stories.setStories(it)
+        })
     }
 
     override fun onDestroyView() {
