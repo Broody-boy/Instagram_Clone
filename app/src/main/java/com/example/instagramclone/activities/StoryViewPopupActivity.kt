940bc72b1acfe2fa9ba4625def9aa007306a5955
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2023-07-30 12:04:03 +0530

    Set Status Bar as black when opening a story

--- app/src/main/java/com/example/instagramclone/activities/StoryViewPopupActivity.kt
+++ app/src/main/java/com/example/instagramclone/activities/StoryViewPopupActivity.kt
@@ -3,6 +3,7 @@ package com.example.instagramclone.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.WindowManager
 import com.bumptech.glide.Glide
 import com.example.instagramclone.R
 import com.example.instagramclone.databinding.ActivityStoryViewPopupBinding
@@ -22,6 +23,10 @@ class StoryViewPopupActivity : AppCompatActivity() {
 
         val list = intent.getSerializableExtra("links") as? ArrayList<String>
         storyLinksList = ArrayList(list)
+
+        val wdw = this.window
+        wdw.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        wdw.statusBarColor = resources.getColor(R.color.black)
 
         setStoryandWait()
     }
